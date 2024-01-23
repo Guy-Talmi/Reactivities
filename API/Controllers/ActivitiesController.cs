b630b7dc3d4b839bfe891a7bbbe701007d1d1442
Guy-Talmi <guytalmi@gmail.com>
2024-01-23 18:36:40 +0200

    End section 9.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,36 +8,43 @@ namespace API.Controllers;
 public class ActivitiesController : BaseApiController
 {
     [HttpGet] // /api/activities
-    public async Task<ActionResult<List<Activity>>> GetActivities()
+    public async Task<IActionResult> GetActivities()
     {
-        return await Mediator.Send(new List.Query());
+        var Result = await Mediator.Send(new List.Query());
+
+        return HandleResult(Result);
     }
 
     [HttpGet("{id}")] // /api/activities/id
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    public async Task<IActionResult> GetActivity(Guid id)
     {
-        return await Mediator.Send(new Details.Query { Id = id });
+        var Result = await Mediator.Send(new Details.Query { Id = id });
+
+        return HandleResult(Result);
     }
 
     [HttpPost] // /api/activities
     public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
     {
-        await Mediator.Send(new Create.Command { Activity = activity });
-        return Ok();
+        var result = await Mediator.Send(new Create.Command { Activity = activity });
+
+        return HandleResult(result);
     }
 
     [HttpPut("{id}")] // /api/activities/id
     public async Task<IActionResult> EditActivity(Guid id, [FromBody] Activity activity)
     {
         activity.Id = id;
-        await Mediator.Send(new Edit.Command { Activity = activity });
-        return Ok();
+        var result = await Mediator.Send(new Edit.Command { Activity = activity });
+
+        return HandleResult(result);
     }
 
     [HttpDelete("{id}")] // /api/activities/id
     public async Task<IActionResult> DeleteActivity(Guid id)
     {
-        await Mediator.Send(new Delete.Command { Id = id });
-        return Ok();
+        var result = await Mediator.Send(new Delete.Command { Id = id });
+
+        return HandleResult(result);
     }
 }
